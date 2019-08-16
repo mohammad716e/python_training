c7e1c8e9e77c3b67f3df635ef2f42940138b7481
mohammad <mohammad716e@gmail.com>
2019-08-17 00:36:40 +0430

    installed faker onlyyyy

--- Django_b/the_second_project_b/home/admin.py
+++ Django_b/the_second_project_b/home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from home.models import Topic,Webpage,Accessrecord
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(Accessrecord)
+
+# the developer super user created by me to test the site and how it is doing
+# Username (leave blank to use 'adminone'): developer
+# Email address: mmohammadianxdev@gmail.com
+# Password: 147741147zxc
+
+# installed faker and iddnt use it then 
+# deactivate
--- Django_b/the_second_project_b/home/models.py
+++ Django_b/the_second_project_b/home/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+# مثلا یک مدل برای اسم تاپیک ها در جنگو ساختیم
+
+class Topic(models.Model):
+    topic_name=models.CharField(max_length=264 , unique=True)
+
+    def __str__(self):
+        return self.topic_name
+
+class Webpage(models.Model):
+    # forign key برای ساخت ستون در کلاس جدید از کلاس دثگر یعنی همان دسته بندی کردن
+    # فکر کنم یعنی این وب پیج هر تپیک که باشه از تاپیک های بالا بگیره
+    topic = models.ForeignKey(Topic)
+    name = models.CharField(max_length=264,unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Accessrecord(models.Model):
+    name = models.ForeignKey(Webpage)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
